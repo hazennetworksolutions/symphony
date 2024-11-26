c7451820f563c31041ccc4512acfae9b3e845035
hazennetworksolutions <161881320+hazennetworksolutions@users.noreply.github.com>
2024-11-26 14:33:38 +0300

    Update 0.4.1-autoscript.sh

--- 0.4.1-autoscript.sh
+++ 0.4.1-autoscript.sh
@@ -222,6 +222,8 @@ printGreen "12. Setting up pruning config..." && sleep 1
 sed -i -e "s/^pruning *=.*/pruning = \"custom\"/" $HOME/.symphonyd/config/app.toml
 sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/" $HOME/.symphonyd/config/app.toml
 sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"10\"/" $HOME/.symphonyd/config/app.toml
+sed -i 's|^prometheus *=.*|prometheus = true|' $HOME/.symphonyd/config/config.toml
+sed -i -e 's|^indexer *=.*|indexer = "null"|' $HOME/.symphonyd/config/config.toml
 
 # Download the snapshot
 # printGreen "12. Downloading snapshot and starting node..." && sleep 1
